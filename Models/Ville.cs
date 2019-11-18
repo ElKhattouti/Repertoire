2342c84322da7245157808f89167b757c6a88b8d
Bilal <bilal@elkhattouti.com>
2019-11-18 15:57:58 +0100

    dataannotation ville et adresse

--- Models/Ville.cs
+++ Models/Ville.cs
@@ -1,12 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+[Table("ville")]
 public class Ville
 {
-
+    [Key]
     public int VilleId { get; set; }
 
+    [Required(ErrorMessage ="Le nom de la ville est obligatoire")]
     public string Nom { get; set; }
 
+    [ForeignKey("pays")]
     public int PaysId { get; set; }
 
 }
