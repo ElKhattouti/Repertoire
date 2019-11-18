e15e76d61ceca8290643450dd0e883819b912f48
Bilal <bilal@elkhattouti.com>
2019-11-18 18:03:32 +0100

    Entreprise et contact

---
+++ Web/Controllers/EntreprisesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Api.Dal;
+
+namespace Web.Controllers
+{
+    public class EntreprisesController : Controller
+    {
+        private readonly RepertoireContext _context;
+
+        public EntreprisesController(RepertoireContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Entreprises
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Entreprise.ToListAsync());
+        }
+
+        // GET: Entreprises/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entreprise = await _context.Entreprise
+                .FirstOrDefaultAsync(m => m.EntrepriseId == id);
+            if (entreprise == null)
+            {
+                return NotFound();
+            }
+
+            return View(entreprise);
+        }
+
+        // GET: Entreprises/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Entreprises/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EntrepriseId,Nom,AdresseId")] Entreprise entreprise)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(entreprise);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(entreprise);
+        }
+
+        // GET: Entreprises/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entreprise = await _context.Entreprise.FindAsync(id);
+            if (entreprise == null)
+            {
+                return NotFound();
+            }
+            return View(entreprise);
+        }
+
+        // POST: Entreprises/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EntrepriseId,Nom,AdresseId")] Entreprise entreprise)
+        {
+            if (id != entreprise.EntrepriseId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(entreprise);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EntrepriseExists(entreprise.EntrepriseId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(entreprise);
+        }
+
+        // GET: Entreprises/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entreprise = await _context.Entreprise
+                .FirstOrDefaultAsync(m => m.EntrepriseId == id);
+            if (entreprise == null)
+            {
+                return NotFound();
+            }
+
+            return View(entreprise);
+        }
+
+        // POST: Entreprises/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var entreprise = await _context.Entreprise.FindAsync(id);
+            _context.Entreprise.Remove(entreprise);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EntrepriseExists(int id)
+        {
+            return _context.Entreprise.Any(e => e.EntrepriseId == id);
+        }
+    }
+}
