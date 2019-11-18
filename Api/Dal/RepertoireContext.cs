7d5e91139dd3f07e6df41afe9ae3112f310055e5
Bilal <bilal@elkhattouti.com>
2019-11-18 16:10:09 +0100

    add VilleContoller

--- Api/Dal/RepertoireContext.cs
+++ Api/Dal/RepertoireContext.cs
@@ -12,6 +12,7 @@ namespace Api.Dal
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql("Host=postgresql-oceanstudio.alwaysdata.net;Database=oceanstudio_entreprise;Username=oceanstudio;Password=bil442.ad");
         public virtual DbSet<Pays> Pays { get; set; }
+        public virtual DbSet<Ville> Ville { get; set; }
 
-    }
+	}
 }
